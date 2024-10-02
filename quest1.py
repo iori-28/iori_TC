a806ad5e36154dd209db007d2dbc71a353ab0a64
iori-28 <skyzyrym17@gmail.com>
2024-10-02 18:00:51 +0700

    Add files via upload

---
+++ quest1.py
@@ -0,0 +1,16 @@
+"""
+Buatkan Data tabel ini  Ke dalam bentuk list of dictionary
+No | Nama | Kelas
+1  | Haikal | IF21B
+2  | Mr. Santai | IF21B
+3  | Kisah | IF22C
+4  | Ajag | IF23B
+"""
+
+Data_list = [{"Nama": "Haikal", "Nama": "Mr. Santai", 
+              "Nama": "Kisah", "Nama": "Ajag"},
+             
+             {"Kelas": "IF21B", "Kelas": "IF21B", 
+              "Kelas": "IF22C", "Kelas": "IF23B"}
+            ]
+
